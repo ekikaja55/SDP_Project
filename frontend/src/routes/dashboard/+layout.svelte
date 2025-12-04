b697ea14d26c674e2a29c79e0356e96750cabe55
ekikaja55 <rizki.a23@mhs.istts.ac.id>
2025-12-04 23:03:17 +0700

    fix lagi 4

--- frontend/src/routes/dashboard/+layout.svelte
+++ frontend/src/routes/dashboard/+layout.svelte
@@ -1,18 +1,35 @@
 <!-- src/routes/dashboard/+layout.ts -->
-<!-- layouting untuk dashboard user / admin -->
 <script lang="ts">
-	export let data;
-	import { logout} from '$lib';
+	import { browser } from '$app/environment';
+	import { jwtDecode } from 'jwt-decode';
 	import SidebarAdmin from '$lib/components/SidebarAdmin.svelte';
 	import SidebarCustomer from '$lib/components/SidebarCustomer.svelte';
+	import { goto } from '$app/navigation';
+
+	let user = null;
+
+	if (browser) {
+		const token = localStorage.getItem("token");
+
+		if (!token) {
+			goto('/login');
+		} else {
+			try {
+				user = jwtDecode(token);
+			} catch {
+				goto('/login');
+			}
+		}
+	}
 </script>
 
 <div class="flex min-h-screen m-0 p-0">
-	{#if data.user?.user_role === 'admin'}
-		<SidebarAdmin onLogout={logout} data={data.user} />
-	{:else}
-		<SidebarCustomer onLogout={logout} data={data.user}/>
+	{#if user?.user_role === 'admin'}
+		<SidebarAdmin data={user} />
+	{:else if user}
+		<SidebarCustomer data={user} />
 	{/if}
+
 	<main class="flex-1 p-6">
 		<slot />
 	</main>
